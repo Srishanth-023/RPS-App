b26ac22003d255b732c85141488b05f1acc9c625
Srishanth <srishanth232007@gmail.com>
2025-08-12 19:56:14 +0530

    Updating for model switch.

--- rps/asgi.py
+++ rps/asgi.py
@@ -3,15 +3,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import game.routing  # Make sure this line exists
+# import game.routing  # Make sure this line exists
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rps.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            game.routing.websocket_urlpatterns  # And that it's being used here
-        )
-    ),
+    # "websocket": AuthMiddlewareStack(
+    #     URLRouter(
+    #         game.routing.websocket_urlpatterns  # And that it's being used here
+    #     )
+    # ),
 })
